60c4d94f57de1e219aa31f34157cf84a857acdcb
ggex86 <mxbalcewicz@gmail.com>
2020-09-16 01:01:46 +0200

    refactoring templates, using base template

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Mike B',
+        'title': 'blog-post1',
+        'content': '1st post content',
+        'date_posted': '15 wrz 2020'
+    },
+    {
+        'author': 'Mike C',
+        'title': 'blog-post2',
+        'content': '2st post content',
+        'date_posted': '16 wrz 2021'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog homepage</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog about page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
