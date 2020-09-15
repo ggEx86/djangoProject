9804ca880464536181606adae1d302c73bc5290f
ggex86 <mxbalcewicz@gmail.com>
2020-09-15 14:31:56 +0200

    added routing for blog, templates folder and basic views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
 
 def home(request):
     return HttpResponse('<h1>Blog homepage</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Blog about page</h1>')
