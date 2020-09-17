be0249ee37a8a63c6d413edd3bfca637797ab99e
ggex86 <mxbalcewicz@gmail.com>
2020-09-17 02:14:59 +0200

    added posts inside database, removed hardcoded context data, registered model for admin

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-# Create your views here.
-posts = [
-    {
-        'author': 'Mike B',
-        'title': 'blog-post1',
-        'content': '1st post content',
-        'date_posted': '15 wrz 2020'
-    },
-    {
-        'author': 'Mike C',
-        'title': 'blog-post2',
-        'content': '2st post content',
-        'date_posted': '16 wrz 2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
